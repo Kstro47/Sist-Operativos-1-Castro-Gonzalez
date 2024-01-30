c8db2317481af2ee47a019b9978f97ec9f8981d0
Kstro47 <castro.andres@correo.unimet.edu.ve>
2024-01-30 14:58:12 -0400

    Test de Worker

---
+++ src/sist/operativo/Worker.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sist.operativo;
+
+/**
+ *
+ * @author yilme
+ */
+public class Worker extends Thread {
+    private int salary;
+    private int type;
+    
+    public Worker(int salary, int type){
+        this.salary = salary;
+        this.type = type;
+    }
+    
+    
+    
+    
+}
